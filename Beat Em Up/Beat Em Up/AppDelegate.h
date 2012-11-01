e8034910c7baa38b35ad0d163d55183d097e469d
Abey Onalaja <abey.onalaja@gmail.com>
2012-11-01 13:39:00 +0000

    iOS project init

---
+++ Beat Em Up/Beat Em Up/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  Beat Em Up
+//
+//  Created by Abideen Onalaja on 01/11/2012.
+//  Copyright Abideen Onalaja 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ Beat Em Up/Beat Em Up/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Beat Em Up
+//
+//  Created by Abideen Onalaja on 01/11/2012.
+//  Copyright Abideen Onalaja 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
